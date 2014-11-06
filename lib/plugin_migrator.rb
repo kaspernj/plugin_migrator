5f0b85df2c61b52dc8c4f7c39dac25ff001cb2e3
kaspernj <k@spernj.org>
2014-11-06 13:05:19 +0100

    Switched to normal autoloading.

--- lib/plugin_migrator.rb
+++ lib/plugin_migrator.rb
@@ -2,9 +2,9 @@ require "string-cases"
 load "#{File.dirname(__FILE__)}/tasks/plugin_migrator_tasks.rake" if ::Kernel.const_defined?(:Rake)
 
 module PluginMigrator
-  def self.const_missing(name)
-    require_relative "plugin_migrator/#{::StringCases.camel_to_snake(name)}"
-    raise LoadError, "Still not loaded: '#{name}'." unless ::PluginMigrator.const_defined?(name)
-    return ::PluginMigrator.const_get(name)
-  end
+  path = "#{File.dirname(__FILE__)}/plugin_migrator"
+
+  autoload :Migration, "#{path}/migration"
+  autoload :MigrationsHelper, "#{path}/migrations_helper"
+  autoload :VersionHelper, "#{path}/version_helper"
 end
